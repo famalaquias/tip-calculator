24d459a6e99aa1fa6a224bb3887117b7c1614881
Fabiane Malaquias <malaquiasfr@gmail.com>
2022-11-15 09:40:40 -0300

    Alterações no Project Calculator

--- scripts.js
+++ scripts.js
@@ -5,8 +5,8 @@ function calculateTips(event) {
   let serviceQualit = document.getElementById('serviceQualit').value;
   let people = document.getElementById('people').value;
 
-  if(bill == '' | serviceQualit == 0) {
-    alert("Por favor, preencha os valores...")
+  if(bill == '' | people <= 1 ) {
+    alert("Por favor, preencha todos os campos...")
     return;
   }
 
@@ -14,13 +14,28 @@ function calculateTips(event) {
     people = 1;
   }
 
+  let totalPeople = bill / people;
+  // totalPeople = totalPeople.toFixed(2);
+
   let total = (bill * serviceQualit) / people;
-  total = total.toFixed(2);
+  // total = total.toFixed(2);
+
+  let totalPay = totalPeople + total;
+  totalPay = totalPay.toFixed(2);
 
   document.getElementById('tip').innerHTML = total;
+  document.getElementById('tipTotal').innerHTML = totalPeople;
+  document.getElementById('pay').innerHTML = totalPay;
+
   document.getElementById('totalTips').style.display = "block";
+  document.getElementById('totalValue').style.display = "block";
+  document.getElementById('totalPay').style.display = "block";
 }
 
 document.getElementById('totalTips').style.display = "none";
+
+document.getElementById('totalValue').style.display = "none";
+
+document.getElementById('totalPay').style.display = "none";
 
 document.getElementById('tipsForm').addEventListener('submit', calculateTips);
